986d99842c95d29c4b16e78f85c2578dfb178c8d
m0ozeh <kcw2205@naver.com>
2023-04-13 06:53:23 +0900

    feat: change layout

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,57 +1,20 @@
 import styled from "styled-components";
-import Category from "./Category";
 import Profile from "./Profile";
-import ProjectWrap from "./ProjectWrap";
+import PinnedProject from "./PinnedProject";
 import SolvedCard from "./SolvedCard";
-
-import { Title } from "../../theme";
-
-const Content = styled.div`
-  * {
-    -webkit-user-select: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -o-user-select: none;
-    user-select: none;
-  }
-  width: var(--layout-width);
-  margin: 3rem auto auto auto;
-`
-
-const LayoutLeft = styled.div`
-  width:var(--layout-left-width);
-  display:flex;
-  justify-content:space-between;
-  flex-direction:column;
-  float:left;
-`
-
-const LayoutRight = styled.div`
-  width:var(--layout-right-width);
-`
-
-const MainWrap = styled.div`
-  display:flex;
-  justify-content:space-between;
-`
+import All from "./all/All";
 
 const Home = (): JSX.Element => {
   return <Content>
-    <Title>#Home</Title>
-    <MainWrap>
-
-      <LayoutLeft>
-        <Profile />
-        <Category />
-      </LayoutLeft>
-      
-      <LayoutRight>
-        <ProjectWrap />
-        <SolvedCard />
-      </LayoutRight>
-
-    </MainWrap>
+    <Profile />
+    <PinnedProject />
+    <All />
   </Content>
 }
+
+const Content = styled.div`
+  width:var(--content-width);
+  margin:0 auto;
+`
 
 export default Home;
