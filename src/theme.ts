bb08fd319185c090e32ba61befae733c4b3e749f
m0ozeh <kcw2205@naver.com>
2023-04-13 06:40:00 +0900

    fix: organize styled components theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,39 @@
 import styled from "styled-components";
 
-const Border = styled.div`
-  border:1px solid #888888;
-  border-radius: 30px;
-  padding:1rem;
-`
-
 const Title = styled.div`
-  font-size:20px;
-  font-weight:600;
-  margin-bottom:30px;
+  margin-top:25px;
+  margin-bottom:20px;
+  font-weight:var(--bold);
+  font-size:var(--font-large);
 `
 
-export { Border, Title };
+const SubTitle = styled(Title)`
+  font-weight:var(--medium);
+`
+
+const Paragraph = styled.div`
+  display:block;
+  margin-left:20px;
+  margin-top:25px;
+  margin-bottom:20px;
+`
+
+const Content = styled.div`
+  * {
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -o-user-select: none;
+    user-select: none;
+  }
+  width: var(--layout-width);
+  margin: 3rem auto auto auto;
+`
+
+const Card = styled.div`
+  border:1px solid var(--comment-color);
+  border-radius: 20px;
+  padding:15px;
+`
+
+export { Title, SubTitle, Content, Paragraph, Card };
