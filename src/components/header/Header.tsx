a5cea460cf833ab421817bfe47af52b6abc79a0f
m0ozeh <kcw2205@naver.com>
2023-04-13 06:49:01 +0900

    feat: add header layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,43 +1,62 @@
 import styled from "styled-components";
-import HomeLogo from "./HomeLogo";
-import MenuList from "./MenuList";
+import HeaderCategoryList from "./HeaderCategoryList";
+import BackgroundImg from "../../assets/heading.png";
+
+const Header = () => {
+  return <Wrap>
+    <div>
+      <Content>
+        <Title>Welcome To My Github Blog!</Title>
+        <div>
+          <div>Watch My Posts</div>
+          <HeaderCategoryList />
+        </div>
+      </Content>
+    </div>
+  </Wrap>
+}
+
+const Wrap = styled.div`
+  width:100%;
+  height:280px;
+  background:url(${BackgroundImg});
+  background-position: center;
+  background-position: 50% 25%;
+  * {
+    font-size:var(--regular);
+    user-select:none;
+  }
+
+  > div {
+    position:relative;
+    height:200px;
+    top:80px;
+  }
+`
+
+const Title = styled.div`
+  font-weight:var(--bold);
+`
 
 const Content = styled.div`
-  /* Header */
-  * {
-    -webkit-user-select: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -o-user-select: none;
-    user-select: none;
+  position:relative;
+  display:flex;
+  flex-direction:column;
+  justify-content:space-between;
+  align-items:center;
+  height:120px;
+  top: 50%;
+  transform: translateY(-50%);
+  
+  > div:nth-child(2) {
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    gap:10px;
+    > div:first-child {
+      font-size:var(--font-large); 
+    }
   }
-  a {
-    text-decoration:none;
-    color:inherit;
-  }
-  width: 100%;
-  height: 80px;
-  box-shadow:0px 4px 4px rgba(0, 0, 0, 0.25);
-`;
-
-const FlexBox = styled.div`
-  margin:0 auto;
-  width: var(--layout-width);
-  height:100%;
-  display: flex;
-  flex-direction: row;
-  align-content: center;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Header = (): JSX.Element => {
-  return <Content>
-    <FlexBox>
-      <HomeLogo />
-      <MenuList/>
-    </FlexBox>
-  </Content>
-}
+`
 
 export default Header;
