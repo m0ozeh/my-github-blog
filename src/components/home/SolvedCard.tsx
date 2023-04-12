3ee168949d07853e290331868706c5dd14e0a6d9
m0ozeh <kcw2205@naver.com>
2023-04-13 06:54:33 +0900

    refactor: change variables

--- src/components/home/SolvedCard.tsx
+++ src/components/home/SolvedCard.tsx
@@ -1,18 +1,18 @@
 import styled from "styled-components";
 
-const Wrap = styled.div`
-  width:45%;
-  float:right;
-`
 const Img = styled.img`
-  width:100%;
+  height:inherit;
+`
+const ImgWrap = styled.div`
+  height:inherit;
+  min-width:257.344px;
 `
 
 const SolvedCard = ():JSX.Element => {
-  return <Wrap>
-    <h2>Solved.ac</h2>
-    <Img src="http://mazassumnida.wtf/api/v2/generate_badge?boj=blu3fishez"></Img>
-  </Wrap>
+  return <ImgWrap>
+      <Img src="http://mazassumnida.wtf/api/v2/generate_badge?boj=blu3fishez"/>
+    </ImgWrap>
+
 }
 
 export default SolvedCard;
